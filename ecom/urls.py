24e7054d98877964c68a071df3361eb441b5969c
padmanabh-27 <padmanabhmadva.27@gmail.com>
2026-02-13 20:40:38 +0530

    removed blog app

--- ecom/urls.py
+++ ecom/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
-    path('blog/',include('blog.urls')),
     
 
 ]
